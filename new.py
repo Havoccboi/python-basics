20d6173e2a2da13461bba1ef288311b903792791
Niloy Rahman <havocisgod@gmail.com>
2020-10-30 06:42:50 +0600

    Update new.py

--- new.py
+++ new.py
@@ -4,7 +4,29 @@ Created on Sat Oct 24 03:28:47 2020
 
 @author: Havoc
 """
-
+"""
 city = ["Kushtia", "Dhaka", "Barishal"]
 for x in range(1,10,2): #range(start,stop,interval)
     print (x)
+    
+    
+
+
+def check_range(i,j):
+    for x in range(i,j):
+        print (x)
+        
+check_range(5,10)
+"""
+
+# for loop and else ,  else blocked to be executed when the for loop is finished
+
+
+for i in range (1, 10, 1):
+    print (i)
+    if i > 3:
+        print("breaking..")
+        break;
+   
+else:
+    print("execution completed")
