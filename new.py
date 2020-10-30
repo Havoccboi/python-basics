97cfc79cc9f4dd1675cc8095548c4e091ecd2d37
Niloy Rahman <havocisgod@gmail.com>
2020-10-30 06:36:21 +0600

    Create new.py

---
+++ new.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 03:28:47 2020
+
+@author: Havoc
+"""
+
+city = ["Kushtia", "Dhaka", "Barishal"]
+for x in range(1,10,2): #range(start,stop,interval)
+    print (x)
